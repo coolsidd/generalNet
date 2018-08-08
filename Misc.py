642c6b6450dc99c8dff0afb558314eec137ad6c2
coolsidd <sidharth_0903@hotmail.com>
2018-08-08 22:33:04 +0530

    added some basic debug functions

---
+++ Misc.py
@@ -0,0 +1,26 @@
+from functools import wraps
+import logging
+import time
+
+
+def logged(orig_func):
+    logging.basicConfig(filename=orig_func.__name__, level=logging.INFO)
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info("{} With args {} and kwargs {}".format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def timed(orig_func):
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(**args, **kwargs)
+        t2 = time.time() - t1
+        print("{} ran in {}".format(orig_func.__name__, t2))
+        return result
+    return wrapper
